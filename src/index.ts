51b88eb74fb0d18f3d2491b267119401b7db81c5
Tom Scanlan <veerscanlan@gmail.com>
2021-12-01 17:54:32 -0600

    update

--- src/index.ts
+++ src/index.ts
@@ -183,3 +183,27 @@ export interface IUserProfile{
     email:string;
     profilePictureThumbnail:string | null;
 }
+
+export interface IHereSearchSuggestion {
+    title: string;
+    id: string;
+    resultType: string;
+    address: IHereAddress;
+    position: IHereAccessEntityOrPosition;
+    access?: (IHereAccessEntityOrPosition)[] | null;
+    distance: number;
+    categories?: (IHereCategoriesEntity)[] | null;
+}
+export interface IHereAddress {
+    label: string;
+}
+export interface IHereAccessEntityOrPosition {
+    lat: number;
+    lng: number;
+}
+export interface IHereCategoriesEntity {
+    id: string;
+    name: string;
+    primary: boolean;
+}
+  
