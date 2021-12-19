a38db4e8bbd4ec1da0b0ad15e5ff43c84352d196
Tom Scanlan <veerscanlan@gmail.com>
2021-12-18 19:43:00 -0600

    update

--- src/instagram/index.ts
+++ src/instagram/index.ts
@@ -1,42 +1,65 @@
 export interface IInstagramMediaSave{
-    datePosted:number;
-    content:string;
-    mediaId:string;
-    posterId:string;
-    displayUrl:string;
-    thumbnail:string;
-    likeCount:string;
+    
 }
 
 export interface IInstagramRoot {
-    total: number
-    medias: IInstagramMedia[]
-  }
-  
-  export interface IInstagramMedia {
-    media_id: string
-    shortcode: string
-    text: string
-    comment_count: IInstagramCommentCount
-    like_count: IInstagramLikeCount
-    display_url: string
-    owner_id: string
-    date: number
-    thumbnail: string
-    thumbnail_resource: IInstagramThumbnailResource[]
-  }
-  
-  export interface IInstagramCommentCount {
-    count: number
-  }
-  
-  export interface IInstagramLikeCount {
-    count: number
-  }
-  
-  export interface IInstagramThumbnailResource {
-    src: string
-    config_width: number
-    config_height: number
-  }
-  
+  node: IInstagramNode
+}
+
+export interface IInstagramNode {
+  comments_disabled: boolean
+  __typename: string
+  id: string
+  edge_media_to_caption: IInstagramEdgeMediaToCaption
+  shortcode: string
+  edge_media_to_comment: IInstagramEdgeMediaToComment
+  taken_at_timestamp: number
+  dimensions: IInstagramDimensions
+  display_url: string
+  edge_liked_by: IInstagramEdgeLikedBy
+  edge_media_preview_like: IInstagramEdgeMediaPreviewLike
+  owner: IInstagramOwner
+  thumbnail_src: string
+  thumbnail_resources: IInstagramThumbnailResource[]
+  is_video: boolean
+  accessibility_caption: string
+}
+
+export interface IInstagramEdgeMediaToCaption {
+  edges: IInstagramEdge[]
+}
+
+export interface IInstagramEdge {
+  node: IInstagramNode2
+}
+
+export interface IInstagramNode2 {
+  text: string
+}
+
+export interface IInstagramEdgeMediaToComment {
+  count: number
+}
+
+export interface IInstagramDimensions {
+  height: number
+  width: number
+}
+
+export interface IInstagramEdgeLikedBy {
+  count: number
+}
+
+export interface IInstagramEdgeMediaPreviewLike {
+  count: number
+}
+
+export interface IInstagramOwner {
+  id: string
+}
+
+export interface IInstagramThumbnailResource {
+  src: string
+  config_width: number
+  config_height: number
+}
